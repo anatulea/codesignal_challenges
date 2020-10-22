ef9731bee4e904e66bef22cd747b9e5386db1c19
Ana Tulea <anatulea@Anas-MacBook-Pro.local>
2020-10-22 12:26:23 -0700

    countBits, matrix

---
+++ matrixElementsSum.py
@@ -0,0 +1,61 @@
+# def matrixElementsSum(matrix):
+#     count = sum(matrix[0])
+#     for i in range(len(matrix)-1):
+#         for j in range(len(matrix[i])):
+#             if matrix[i][j] == 0:
+#                 print('whwhhw')
+#                 # print(f'matrix[i][j]: {matrix[i][j]})'
+#                 # return
+#             else:
+#                 print(f'matrix[i+1][j]:{matrix[i+1][j]}')
+#                 count = count+ matrix[i+1][j]
+#     return count
+
+# def matrixElementsSum(matrix):
+#     count = 0
+#     for i in range(len(matrix)):
+#         print(f'i:{i}')
+#         for idx, num in enumerate(matrix[i]):
+#             # print(f'idx:{idx} num{num}')
+#             if num == 0 :
+#                  for(int k=i;k<matrix.length;k++){
+#                     matrix[k][j]=0;
+#                 }
+#                 continue;
+#                 # print(f'idx:{idx} num{num}')
+#                 # print(f'matrix[i+1][idx]:{matrix[i+1][idx]}')
+#                 # # matrix[i+1][idx] = 0
+#             else:
+#                 # print(f'num:{num}')
+#                 count = count + num
+                
+#     return count
+
+# def matrixElementsSum(matrix):
+#     count = 0
+#     for i in range(len(matrix)):
+#         for j in range(len(matrix[i])):
+#             if matrix[i][j] == 0:
+#                 k = i+1
+#                 for k in range(len(matrix)):
+#                     matrix[k][j] = 0
+#     for x in range(len(matrix)):
+#         for y in range(len(matrix[x])):
+#             count += matrix[x][k]         
+#     return count
+def matrixElementsSum(m):
+    r = len(m)
+    c = len(m[0])
+    total=0
+    for j in range(c):
+        for i in range(r): 
+            if m[i][j]!=0:
+                total+=m[i][j]
+            else:
+                break
+    return total
+
+mymatrix =[ [1,1,1,0], 
+            [0,5,0,1], 
+            [2,1,3,10]]
+print(matrixElementsSum(mymatrix))
---
+++ test.py
@@ -0,0 +1,11 @@
+xs =[()]
+res = [False]*2
+# print(res)
+if xs:
+    res[0]=True
+if xs[0]:
+    res[1]=True
+# print(res) # [True, False]
+def division(x,y):
+    return x/y
+print(division(15,-4) )    
