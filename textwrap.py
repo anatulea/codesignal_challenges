c3ab87af2d9cdc3da28e7753d6d19283c74f8cb7
Ana Tulea <anatulea@Anas-MacBook-Pro.local>
2020-10-22 20:22:44 -0700

    textWrap used

---
+++ textwrap.py
@@ -0,0 +1,22 @@
+"""
+ Implement a function that, given a feedback and the size of the screen, splits the feedback into lines so that:
+
+each token (i.e. sequence of non-whitespace characters) belongs to one of the lines entirely;
+each line is at most size characters long;
+no line has trailing or leading spaces;
+each line should have the maximum possible length, assuming that all lines before it were also the longest possible.
+Example
+
+For feedback = "This is an example feedback" and size = 8,
+the output should be
+
+feedbackReview(feedback, size) = ["This is", 
+                                  "an", 
+                                  "example", 
+                                  "feedback"]
+
+"""
+import textwrap
+
+def feedbackReview(feedback, size):
+    return textwrap.TextWrapper(size).wrap(feedback)
